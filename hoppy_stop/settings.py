66850c794d8def6bd944e64bdf2c8499689dc42c
antoniotorone <antoniotorone8913@gmail.com>
2021-10-15 19:20:09 +0000

    navbar added to base.html

--- hoppy_stop/settings.py
+++ hoppy_stop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-69*&q2$#*og-9^+3m!2-fjmv&o5e_l#@5467ccob3kcwgm5c&f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -147,6 +147,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
