6b2af0fc24bdb9ade88efad10185af48c0a3de6e
antoniotorone <antoniotorone8913@gmail.com>
2021-11-25 21:11:50 +0000

    checkout form data caching added in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,6 +17,8 @@ class StripeWH_Handler:
     def handle_payment_intent_succeeded(self, event):
         """ Take care about the payment_intent.succeeded webhook from stripe """
 
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
