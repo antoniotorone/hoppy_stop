88befc9504ec684aa663c9f6d603e097eede9290
antoniotorone <antoniotorone8913@gmail.com>
2021-11-29 16:09:53 +0000

    superuser created

--- hoppy_stop/settings.py
+++ hoppy_stop/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-69*&q2$#*og-9^+3m!2-fjmv&o5e_l#@5467ccob3kcwgm5c&f'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,13 +123,6 @@ WSGI_APPLICATION = 'hoppy_stop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#       'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
 
 if "DATABASE_URL" in os.environ:
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
